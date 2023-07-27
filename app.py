0e860291557dfb9793a5472464d6a4b6fb464a79
Florencia Melgar <flormelgar054@gmail.com>
2023-07-27 12:01:05 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = """
+Juan 
+Pedro
+Jose
+"""
+x, y = 1, 2
+a = b = 1
+print(type(students_count))
+age: int = 20
+age = 'python'
+print(age)
+x = 1
+print(id(x))
+x = x + 1
+
---
+++ string.py
@@ -0,0 +1,29 @@
+course = 'python programacion'
+print(len(course))
+print(course[0])
+print(course[-2])
+print(course[0:3])
+print(course[:3])
+print(course[0:])
+print(course[:])
+print(id(course))
+print(id(course[0]))
+message = "python \"pragrama"
+print(message)
+
+first = 'Florencia'
+last = 'Melgar'
+full = first + " " + last
+print(full)
+full = f'{first} {last}'
+print(full)
+print(full.upper())
+print(full.lower())
+print(full.title())
+full = ' hola feo'
+print(full.strip())
+print(course.find('pro'))
+print(full.find('fe'))
+print(full.replace('o', '#'))
+print('hola' in full)
+
